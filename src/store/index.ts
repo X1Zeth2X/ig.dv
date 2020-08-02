c693eea4d35676908bbe550d5283a4d01d067d7c
Zeth Leonardo <zeth.leonardo@protonmail.com>
2020-08-02 16:24:32 +0400

    Added Instagram data set.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import Vue from 'vue'
 import Vuex from 'vuex'
 
@@ -5,11 +6,48 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    // Initialize empty states for Instagram..
+    searches: [],
+    shopping: [],
+    settings: {},
+    information_about_you: {},
+    stories_activities: {},
+    saved: [],
+    seen_content: {},
+    devices: {},
+    likes: {},
+    fundRaisers: [],
+    profile: {},
+    guides: [],
+    messages: [],
+    connections: {},
+    account_history: {},
+    comments: {},
+    autofill: [],
+    uploaded_contacts: [],
+
+    // Initialize empty feed data
+    feed: {}
   },
+
   mutations: {
+    setInstagramData (state, { key, value }: {
+      key: keyof typeof state;
+      value: any;
+    }) {
+      state[key] = value;
+    },
   },
+
   actions: {
+    setInstagramData(context, payload) {
+      context.commit('setInstagramData', {
+        key: payload.key,
+        value: payload.value
+      });
+    },
   },
+
   modules: {
   }
 })

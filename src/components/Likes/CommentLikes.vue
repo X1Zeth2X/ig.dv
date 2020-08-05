ec52ab64236fa5c46e5a02a09cd196c0bfbb5a74
Zeth Leonardo <zeth.leonardo@protonmail.com>
2020-08-05 17:05:07 +0400

    Added Likes components.

---
+++ src/components/Likes/CommentLikes.vue
@@ -0,0 +1,64 @@
+<template>
+<div id="mediaLikes">
+  <div class="card" v-for="like in currentFeed" :key="like[0]">
+    <div class="card-content">
+      You liked <span class="has-text-weight-bold" v-text="like[1]"/>'s
+      comment on {{ like[0] }}.
+    </div>
+  </div>
+
+  <hr />
+  <div class="bottom">
+    <a @click="goTop"
+      class="has-text-weight-bold"
+    >Go back to Top</a>
+
+    <br>
+    <b-button
+      style="margin-top: 15px;"
+      class="is-primary"
+      @click="showMore"
+      rounded
+      size="is-large"
+      icon-right="chevron-down"
+    />
+  </div>
+</div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import { Getter, Action } from 'vuex-class';
+
+@Component
+export default class MediaLikes extends Vue {
+  @Getter
+  private currentFeed!: Array<[]>;
+
+  @Action
+  private setFeed!: Function;
+
+  mounted() {
+    this.loadCommentLikes();
+  }
+
+  private loadCommentLikes = async () => {
+    const commentLikes: Array<[]> = this.$store.getters.likes.comment_likes;
+    await this.$store.dispatch('setFeed', commentLikes.slice(0, 10));
+  }
+
+  showMore = async () => {
+    // Check what's the current tab
+    // 0 - Media, 1 - Comment
+    const currentFeed: Array<[]> = this.$store.getters.currentFeed;
+    const commentLikes: Array<[]> = this.$store.getters.likes.comment_likes;
+    const data: Array<[]> = commentLikes.slice(currentFeed.length, currentFeed.length + 10);
+
+    await this.setFeed(commentLikes.concat(data));
+  }
+
+  goTop = () => {
+    window.scrollTo(0, 0);
+  }
+}
+</script>
---
+++ src/components/Likes/MediaLikes.vue
@@ -0,0 +1,64 @@
+<template>
+<div id="mediaLikes">
+  <div class="card" v-for="like in currentFeed" :key="like[0]">
+    <div class="card-content">
+      You liked <span class="has-text-weight-bold" v-text="like[1]"/>'s
+      post on {{ like[0] }}.
+    </div>
+  </div>
+
+  <hr />
+  <div class="bottom">
+    <a @click="goTop"
+      class="has-text-weight-bold"
+    >Go back to Top</a>
+
+    <br>
+    <b-button
+      style="margin-top: 15px;"
+      class="is-primary"
+      @click="showMore"
+      rounded
+      size="is-large"
+      icon-right="chevron-down"
+    />
+  </div>
+</div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import { Getter, Action } from 'vuex-class';
+
+@Component
+export default class MediaLikes extends Vue {
+  @Getter
+  private currentFeed!: Array<[]>;
+
+  @Action
+  private setFeed!: Function;
+
+  created() {
+    this.loadMediaLikes();
+  }
+
+  private loadMediaLikes = async () => {
+    const mediaLikes: Array<[]> = this.$store.getters.likes.media_likes;
+    await this.setFeed(mediaLikes.slice(0, 10));
+  }
+
+  showMore = async () => {
+    // Check what's the current tab
+    // 0 - Media, 1 - Comment
+    const currentFeed: Array<[]> = this.$store.getters.currentFeed;
+    const mediaLikes: Array<[]> = this.$store.getters.likes.media_likes;
+    const data: Array<[]> = mediaLikes.slice(currentFeed.length, currentFeed.length + 10);
+
+    await this.setFeed(mediaLikes.concat(data));
+  }
+
+  goTop = () => {
+    window.scrollTo(0, 0);
+  }
+}
+</script>

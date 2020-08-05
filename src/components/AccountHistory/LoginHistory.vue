1caef21a3bca1bdcc5e1d6b325f3ebecf894dd9a
Zeth Leonardo <zeth.leonardo@protonmail.com>
2020-08-05 17:06:20 +0400

    Added ability to view IP and details.

--- src/components/AccountHistory/LoginHistory.vue
+++ src/components/AccountHistory/LoginHistory.vue
@@ -1,7 +1,53 @@
 <template>
 <div class="card">
   <div class="card-content">
-    Cookie
+    <ul class="is-size-6">
+      <li>
+        <b-icon icon="cookie"/>
+        <span class="has-text-weight-bold"> Cookie: </span>
+        {{ data.cookie_name }}
+      </li>
+
+      <li>
+        <b-icon icon="ip-network"/>
+        <span class="has-text-weight-bold"> IP Address: </span>
+        {{ data.ip_address }}
+
+        <b-button 
+          outlined
+          rounded
+          class="is-info"
+          icon-right="magnify"
+          size="is-small"
+          style="float: right;"
+          @click="viewIP(data.ip_address)"
+        >View on LookIP</b-button>
+      </li>
+
+      <li>
+        <b-icon icon="earth"/>
+        <span class="has-text-weight-bold"> Language Code: </span>
+        {{ data.language_code }}
+      </li>
+
+      <li>
+        <b-icon icon="calendar"/>
+        <span class="has-text-weight-bold"> Timestamp: </span>
+        {{ data.timestamp }}
+      </li>
+
+      <li>
+        <b-icon icon="devices"/>
+        <span class="has-text-weight-bold"> User Agent: </span>
+        {{ data.user_agent }}
+      </li>
+
+      <li v-show="data.device_id">
+        <b-icon icon="card-account-details"/>
+        <span class="has-text-weight-bold"> Device ID: </span>
+        {{ data.device_id }}
+      </li>
+    </ul>
   </div>
 </div>
 </template>
@@ -12,5 +58,9 @@ import { Vue, Component, Prop } from 'vue-property-decorator';
 @Component
 export default class LoginHistory extends Vue {
   @Prop() data!: object;
+
+  viewIP = (IP: string) => {
+    window.open(`https://www.lookip.net/ip/${ IP }`, '_blank', 'nodeIntegration=no');
+  }
 }
 </script>

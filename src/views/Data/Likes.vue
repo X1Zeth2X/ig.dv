9a06b721d14f5d5eedf56373b52ce1d6fa3f84d2
Zeth Leonardo <zeth.leonardo@protonmail.com>
2020-08-05 17:05:28 +0400

    Added Likes category view.

---
+++ src/views/Data/Likes.vue
@@ -0,0 +1,53 @@
+<template>
+<div class="container" id="comments">
+  <PageHeader
+    :title="'Likes'"
+    :desc="'Likes you have made on comments and media.'"
+    :showBack="true"
+  />
+
+
+
+  <div class="card styled-card">
+    <div class="card-content">
+      <!-- Tabs -->
+      <b-tabs type="is-toggle" expanded v-model="currentTab">
+        <b-tab-item
+          label="Media Likes"
+          icon="post"
+        />
+        <b-tab-item
+          label="Comment likes"
+          icon="comment-text-outline"
+        />
+      </b-tabs>
+
+      <component :is="currentTab === 0 ? 'MediaLikes' : 'CommentLikes'" />
+
+
+    </div>
+  </div>
+
+
+</div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Watch } from 'vue-property-decorator';
+import { Getter, Action } from 'vuex-class';
+
+import PageHeader from '@/components/PageHeader.vue';
+import MediaLikes from '@/components/Likes/MediaLikes.vue';
+
+@Component({
+  components: {
+    PageHeader,
+    MediaLikes,
+    'CommentLikes': () => import('@/components/Likes/CommentLikes.vue')
+  }
+})
+export default class Comments extends Vue {
+  private currentTab = 0;
+  private currentComponent = 'MediaLikes';
+}
+</script>

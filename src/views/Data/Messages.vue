ebb29eb37100e450c6591c993d27306d3345f8f2
Zeth Leonardo <zeth.leonardo@protonmail.com>
2020-08-13 01:02:54 +0400

    Redesign messages.

--- src/views/Data/Messages.vue
+++ src/views/Data/Messages.vue
@@ -6,9 +6,59 @@
     :showBack="true"
   />
 
-  <div class="card styled-card">
+  <div class="card">
     <div class="card-content">
-      Will be last to be implemented, it's quite complicated.
+      <div class="is-size-5" style="margin-bottom: .5em;">
+        You have a total of
+        <span class="has-text-weight-bold">{{ totalDMs }}</span>
+        DMs (showing {{ currentFeed.length }}):
+      </div>
+
+      <div
+        class="card"
+        v-for="(dm, index) in currentFeed"
+        :key="index"
+      >
+        <div class="card-content">
+          <div class="columns">
+            <div class="column">
+              Conversations:
+
+              <ul>
+                <li
+                  v-for="(participant, index) in dm.participants"
+                  :key="index"
+                >{{ participant }}</li>
+              </ul>
+
+            </div>
+          </div>
+
+          <b-button>
+            View
+          </b-button>
+        </div>
+      </div>
+
+      <div class="bottom">
+        <hr />
+
+        <a @click="goTop"
+          class="has-text-weight-bold"
+        >Go back to Top</a>
+
+        <br>
+        <b-button
+          style="margin-top: 15px;"
+          class="is-primary"
+          rounded
+          size="is-large"
+          icon-right="chevron-down"
+
+          @click="showMore"
+          v-show="currentFeed.length !== totalDMs"
+        />
+      </div>
     </div>
   </div>
 </div>
@@ -16,7 +66,7 @@
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
-import { Getter } from 'vuex-class';
+import { Getter, Action } from 'vuex-class';
 
 import PageHeader from '@/components/PageHeader.vue';
 
@@ -26,7 +76,41 @@ import PageHeader from '@/components/PageHeader.vue';
   }
 })
 export default class Messages extends Vue {
+  private totalDMs = 0;
+
   @Getter
-  private DMs!: object;
+  private currentFeed!: object[];
+
+  @Action
+  private setFeed!: Function;
+
+  async created() {
+    const DMs: object[] = this.$store.getters.DMs;
+
+    this.totalDMs = DMs.length;
+    await this.setFeed(DMs.slice(0, 10));
+  }
+
+  private goTop = () => {
+    window.scrollTo(0, 0);
+  }
+
+  private showMore = async () => {
+    const DMs: object[] = this.$store.getters.DMs;
+    const currentFeed: object[] = this.$store.getters.currentFeed;
+    const data = DMs.slice(currentFeed.length, currentFeed.length + 10);
+
+    await this.setFeed(currentFeed.concat(data));
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.message {
+  padding: .5em;
+  word-wrap: break-word;
+  overflow: hidden;
+  width: 100%;
+  border-radius: 1em;
+}
+</style>
